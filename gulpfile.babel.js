64dccb395daea1de126cbc9942052ab5e5496cfa
msmpunk <msm_010_27_12@hotmail.com>
2018-12-15 23:42:25 -0600

    + Update gulp to version 4

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -9,50 +9,50 @@ import sourcemaps from 'gulp-sourcemaps'
 import sass from 'gulp-sass'
 import browserSync from 'browser-sync';
 
+const watchServer = () => {
+  browserSync.init({
+    server: "./public"
+  });
+  gulp.watch("./src/sass/*.sass").on('change', browserSync.reload);
+  gulp.watch("./src/pug/*.pug").on('change', browserSync.reload);
+}
 
-gulp.task('serve', ['sass'], () => {
-    browserSync.init({
-        server: "./public"
-    });
-    gulp.watch("./src/sass/*.sass", ['sass']);
-    gulp.watch("src/pug/*.pug").on('change', browserSync.reload);
-});
+const watchPug = () =>
+  gulp.src('./src/pug/**/*.pug')
+      .pipe(pug())
+      // .pipe(pug({pretty: true}))
+      .pipe(gulp.dest('public'))
+      .pipe(browserSync.stream());
 
-gulp.task('default', ['serve'], () => {
-    gulp.watch(['./src/pug/**/*.pug'],obj => { gulp.start('views') });
-    gulp.watch(['./src/js/**/*.+(js|json)'],obj => { gulp.start('js') });
-    gulp.watch(['./src/sass/**/*.sass'], obj => { gulp.start('sass') });
-});
 
-gulp.task('sass', function() {
-    return gulp.src('./src/sass/**/*.sass')
-        .pipe(sass())
-        .pipe(cleanCSS())
-        .pipe(sourcemaps.write('./css/'))
-        .pipe(gulp.dest('public/css'))
-        .pipe(browserSync.stream());
-});
+const watchSass = () =>
+  gulp.src('./src/sass/**/*.sass')
+    .pipe(sass())
+    .pipe(cleanCSS())
+    .pipe(sourcemaps.write('./css/'))
+    .pipe(gulp.dest('public/css'))
+    .pipe(browserSync.stream());
 
-gulp.task('views',() => {
-    return gulp.src('./src/pug/**/*.pug')
-        .pipe(pug())
-        // .pipe(pug({pretty: true}))
-        .pipe(gulp.dest('public'))
-        .pipe(browserSync.stream());
-});
+const watchJs = () =>
+  browserify('./src/js/index.js')
+    .transform('babelify', {
+        presets: ['env']
+    })
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.write('./js/'))
+    .pipe(jsmin())
+    .pipe(gulp.dest('public/js'))
+    .pipe(browserSync.stream());
 
-gulp.task('js', function () {
-    return browserify('./src/js/index.js')
-        .transform('babelify', {
-            presets: ['env']
-        })
-        .bundle()
-        .pipe(source('bundle.js'))
-        .pipe(buffer())
-        .pipe(sourcemaps.write('./js/'))
-        .pipe(jsmin())
-        .pipe(gulp.dest('public/js'))
-        .pipe(browserSync.stream());
-});
+// Watch files
+const watchFiles = () => {
+  gulp.watch('./src/pug/**/*.pug', watchPug);
+  gulp.watch('./src/sass/**/*.sass', watchSass);
+  gulp.watch('./src/js/**/*.+(js|json)',watchJs);
+}
+
+gulp.task("dev", gulp.parallel(watchFiles, watchServer));
 
 
